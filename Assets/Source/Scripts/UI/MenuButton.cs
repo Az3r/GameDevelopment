63c1a5ef02fdf67330aa4124f8c5ea0ca751ffc6
Nguyen Manh Tuan - 1712875 <azerprojects@gmail.com>
2021-01-04 15:17:37 +0700

    Move project files to Source folder

---
+++ Assets/Source/Scripts/UI/MenuButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+  public void OnPointerEnter(PointerEventData eventData)
+  {
+    transform.localScale = new Vector3(1.02f, 1.02f, 1.02f);
+  }
+
+  public void OnPointerExit(PointerEventData eventData)
+  {
+    transform.localScale = new Vector3(1f, 1f, 1f);
+  }
+}
