7cdc0593906d03f6d7b932492f970261c1466ba9
Minh Tuan <xlightrayx@gmail.com>
2021-01-08 07:24:02 +0700

    add ...

---
+++ Assets/Source/Scripts/Controllers/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	private float tileSizeY;
+
+	private Vector3 startPosition;
+
+	void Start()
+	{
+		startPosition = transform.position;
+		tileSizeY = transform.localScale.x/10;
+	}
+
+	void Update()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+		transform.position = startPosition - Vector3.up * newPosition;
+	}
+}
---
+++ Assets/Source/Scripts/Controllers/ExplosionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    public float lifetime = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DestroyObject(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
