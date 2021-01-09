14e08cbff3828e6aa4eedcc4cdb37e8e2f2e5799
Nguyen Manh Tuan - 1712875 <azerprojects@gmail.com>
2021-01-09 15:51:25 +0700

    + rebind key per session only

--- Assets/Source/Scripts/UI/MenuController.cs
+++ Assets/Source/Scripts/UI/MenuController.cs
@@ -8,76 +8,75 @@ using TMPro;
 
 public class MenuController : MonoBehaviour
 {
-  public AudioMixer audioMixer;
-  public TMP_Dropdown graphics;
-  public TMP_Dropdown resolutions;
+    public AudioMixer audioMixer;
+    public TMP_Dropdown graphics;
+    public TMP_Dropdown resolutions;
 
-  public List<GameObject> fullScreenToggle;
-  public List<GameObject> menus;
-
-  private void Start()
-  {
-    graphics.value = QualitySettings.GetQualityLevel();
-
-    // fullscreen
-    bool fullscreen = Screen.fullScreen;
-    fullScreenToggle[0].SetActive(fullscreen);
-    fullScreenToggle[1].SetActive(!fullscreen);
-
-    // resolutions
-    List<string> options = new List<string>();
-    Resolution current = Screen.currentResolution;
-    int currentIndex = 0;
-    for (int i = 0; i < Screen.resolutions.Length; i++)
+    public List<GameObject> fullScreenToggle;
+    public List<GameObject> menus;
+    private void Start()
     {
-      var res = Screen.resolutions[i];
-      options.Add($"{res.width}x{res.height}");
+        graphics.value = QualitySettings.GetQualityLevel();
 
-      if (res.width == current.width && res.height == current.height)
-      {
-        currentIndex = i;
-      }
+        // fullscreen
+        bool fullscreen = Screen.fullScreen;
+        fullScreenToggle[0].SetActive(fullscreen);
+        fullScreenToggle[1].SetActive(!fullscreen);
+
+        // resolutions
+        List<string> options = new List<string>();
+        Resolution current = Screen.currentResolution;
+        int currentIndex = 0;
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            var res = Screen.resolutions[i];
+            options.Add($"{res.width}x{res.height}");
+
+            if (res.width == current.width && res.height == current.height)
+            {
+                currentIndex = i;
+            }
+        }
+        resolutions.AddOptions(new List<string>(options));
+        resolutions.value = currentIndex;
     }
-    resolutions.AddOptions(new List<string>(options));
-    resolutions.value = currentIndex;
-  }
-  public void StartGame()
-  {
-    SceneManager.LoadScene("MainScene");
-  }
-
-  public void ExitGame()
-  {
-    Application.Quit();
-  }
-
-  public void OpenMenu(int index)
-  {
-    for (int i = 0; i < menus.Count; i++)
+    public void StartGame()
     {
-      menus[i].SetActive(false);
+        SceneManager.LoadScene("MainScene");
     }
-    menus[index].SetActive(true);
-  }
 
-  public void SetVolumn(float value)
-  {
-    audioMixer.SetFloat("VolumnValue", value);
-  }
-  public void SetGraphic(int quality)
-  {
-    QualitySettings.SetQualityLevel(quality);
-  }
-  public void SetFullScreen(bool value)
-  {
-    Screen.fullScreen = value;
-    fullScreenToggle[0].SetActive(value);
-    fullScreenToggle[1].SetActive(!value);
-  }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 
-  public void SetResolution(int index)
-  {
-    Resolution selected = Screen.resolutions[index];
-    Screen.SetResolution(selected.width, selected.height, Screen.fullScreen);
-  }
+    public void OpenMenu(int index)
+    {
+        for (int i = 0; i < menus.Count; i++)
+        {
+            menus[i].SetActive(false);
+        }
+        menus[index].SetActive(true);
+    }
+
+    public void SetVolumn(float value)
+    {
+        audioMixer.SetFloat("VolumnValue", value);
+    }
+    public void SetGraphic(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+    public void SetFullScreen(bool value)
+    {
+        Screen.fullScreen = value;
+        fullScreenToggle[0].SetActive(value);
+        fullScreenToggle[1].SetActive(!value);
+    }
+
+    public void SetResolution(int index)
+    {
+        Resolution selected = Screen.resolutions[index];
+        Screen.SetResolution(selected.width, selected.height, Screen.fullScreen);
+    }
 }
