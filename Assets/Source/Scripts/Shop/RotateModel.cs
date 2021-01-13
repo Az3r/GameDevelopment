d8dcc483a8c6bc03df5dd4031a09f6f2f124509a
Nguyen Manh Tuan - 1712875 <azerprojects@gmail.com>
2021-01-13 15:30:47 +0700

    + select spacecraft scene

---
+++ Assets/Source/Scripts/Shop/RotateModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateModel : MonoBehaviour
+{
+    private Transform model;
+    public Vector3 angle;
+    private void Start()
+    {
+        model = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        model.Rotate(angle);
+    }
+}
