ccb7f0d2f92a55dde382c0c36c35dcaa78c7622c
Nguyen Manh Tuan - 1712875 <azerprojects@gmail.com>
2021-01-13 13:41:58 +0700

    + shoppping scene: click item

---
+++ Assets/Source/Scripts/Shop/ShopColorItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShopColorItem : MonoBehaviour, IPointerDownHandler
+{
+    private new MeshRenderer renderer;
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("hello");
+        SpaceCraftStore.Instance.SetSpaceCraft(renderer.material);
+    }
+}
---
+++ Assets/Source/Scripts/Shop/ShopModelItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopModelItem : MonoBehaviour
+{
+    private MeshFilter filter;
+    private void Start()
+    {
+        filter = GetComponent<MeshFilter>();
+    }
+    private void OnMouseUp()
+    {
+        SpaceCraftStore.Instance.SetSpaceCraft(filter.mesh);
+    }
+}
