35bec168ea7a11ed1f18e350b9d4fbabcc03100f
Nguyen Manh Tuan - 1712875 <azerprojects@gmail.com>
2021-01-17 15:42:43 +0700

    + saved data model

--- Assets/Source/Scripts/Controllers/GlobalState.cs
+++ Assets/Source/Scripts/Controllers/GlobalState.cs
@@ -1,17 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class GlobalState : MonoBehaviour
 {
+    public static GlobalState Instance;
     public GameObject[] spacecrafts;
     public GameObject SelectedSpaceCraft => spacecrafts[selectedModelIndex];
     public int selectedModelIndex = 0;
-    public static GlobalState Instance;
-    // Start is called before the first frame update
+
+    public List<SavedData> saveds = new List<SavedData>();
+
     private void Awake()
     {
         Instance = this;
         DontDestroyOnLoad(this);
+    }
+
+    private void Start()
+    {
+        LoadSaveFiles();
+    }
+
+    private void LoadSaveFiles()
+    {
+        foreach (var file in Directory.EnumerateFiles("saves"))
+        {
+            saveds.Add(SavedData.Load(file));
+        }
     }
 }
---
+++ Assets/Source/Scripts/Models/SavedData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class SavedData
+{
+    public int slot;
+    public int currentStage;
+    public int modelIndex;
+    public int money;
+    public int attackLevel;
+    public int healthLevel;
+    public int reloadLevel;
+
+    public static SavedData Load(string name)
+    {
+        try
+        {
+            var path = Path.Combine("saves", name);
+            var json = File.ReadAllText(path);
+            return JsonUtility.FromJson<SavedData>(json);
+        }
+        catch (System.Exception)
+        {
+            return null;
+        }
+    }
+    public bool Save(int slot)
+    {
+        try
+        {
+            this.slot = slot;
+            var path = Path.Combine("saves", slot.ToString("###"));
+            var json = JsonUtility.ToJson(this);
+            File.WriteAllText(path, json);
+            return true;
+        }
+        catch (System.Exception)
+        {
+            return false;
+        }
+    }
+}
